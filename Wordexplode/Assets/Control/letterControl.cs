e11e2618c0bc60df17a7969890f4bc28e3fd61d3
timhongok <timhongok@gmail.com>
2015-10-03 10:35:50 -0700

    Unity
    Create A-Z prefab

---
+++ Wordexplode/Assets/Control/letterControl.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class letterControl : MonoBehaviour {
+
+	public GameObject particle;
+	GameObject camera;
+	public float speed;
+	public GameObject ascii;
+	string name;
+	// Use this for initialization
+	void Start () {
+		camera = GameObject.FindWithTag("MainCamera");
+		name = this.gameObject.name;
+		speed = Random.Range (0.3f, 1.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown(name)) {
+			particle.SetActive(true);
+			Destroy(this.gameObject,1.0f);
+			ascii.SetActive(false);
+		}
+
+		transform.position = Vector3.MoveTowards (transform.position, camera.transform.position, speed);
+	}
+}
